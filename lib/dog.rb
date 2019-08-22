c53a78ef8e8ccaf3bbd629a46f63d9f001f76b4f
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-22 03:13:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,6 @@
 class Dog
-  def initialize(name)
-    @name= name
-    
-  end 
+  def name
+    @name
 end
   
 def name 
@@ -11,7 +9,11 @@ end
 def name=(dog_name) 
   @name = dog_name 
 end
+ end
  
-  fido = Dog.new ("fido")
-  #fido.name = "fido"
-  fido.name
+ def bark
+   puts "woof!"
+ end
+  fido = Dog.new
+  fido.name = "fido"
+  fido.bark 
